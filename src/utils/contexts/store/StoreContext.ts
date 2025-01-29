e163ffff4a09a18308873f058941344aba1c7dc7
evgeny <hurbirate@yandex.ru>
2025-01-29 14:31:34 +0700

    modified interfaces in storecontext

--- src/utils/contexts/store/StoreContext.ts
+++ src/utils/contexts/store/StoreContext.ts
@@ -1,16 +1,31 @@
 import { createContext } from 'react';
 
-interface AppContextType {
-  sharedData: SharedDataProps; // замените any на конкретный тип ваших данных
-  setSharedData: (data: any) => void;
-}
-
 interface SharedDataProps {
   seanceInfo?: {
     seanceDate: string;
     seanceTime: string;
     hallName: string;
   };
+  selectedSeats?: {
+    price: number;
+    id: number;
+    seatId: number;
+  }[];
+}
+
+interface FormDataProps {
+  firstName: string;
+  lastName: string;
+  phone: number;
+  email: string;
+  adress: string;
+}
+
+interface AppContextType {
+  sharedData: SharedDataProps; // замените any на конкретный тип ваших данных
+  setSharedData: (data: SharedDataProps) => void;
+  formData: FormDataProps;
+  setFormData: (data: FormDataProps) => void;
 }
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
